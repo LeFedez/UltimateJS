d3903130ffdad734431aabfff502c1d1e41df39b
Leonardo Fernandez <leofernandez03@gmail.com>
2024-02-03 15:59:01 -0300

    segundo commit 7-arrays

--- 01-tipos/05-comentarios.js
+++ 01-tipos/05-comentarios.js
@@ -1,4 +1,11 @@
 // nota de numero
+/* todo dentro de estos slash es un comentario*/
+/**
+ * este
+ * es 
+ * un
+ * comentario
+ */
 let numero = 45;
 
 console.log(numero);
---
+++ 01-tipos/07-array.js
@@ -0,0 +1,4 @@
+let animales = ['cerdo', 'caballo'];
+
+console.log(animales);
+console.log(animales[0]);
